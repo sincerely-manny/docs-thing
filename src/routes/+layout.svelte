79f7f714b9b7c0dcdce8e5c63990b1714a9b8a7b
Kirill <sincerely.manny@gmail.com>
2023-11-02 13:03:20 +0300

    removed boilerplate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script>
-	import Header from './Header.svelte';
 	import '../app.css';
 </script>
-
-<Header />
 
 <main>
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
