8bc0e75c5dca7aedc297630aba9e13774386811b
Kirill <sincerely.manny@gmail.com>
2024-01-14 21:52:51 +0300

    require -> import

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+import fs from 'fs';
+
 const dir = 'node_modules/pdfjs-dist/es5/build/pdf.js';
 const content = fs.readFileSync(dir, { encoding: 'utf-8' });
 fs.writeFileSync(dir, content.replace('"./pdf.worker.js";', `__dirname + "/pdf.worker.js";`));
