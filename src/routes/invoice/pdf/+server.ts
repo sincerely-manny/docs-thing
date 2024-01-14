b9245d9e0712d99c2005a35e9b3620cd9ad1fd31
Kirill <sincerely.manny@gmail.com>
2024-01-14 19:40:28 +0300

    dynanic pdf gen from db

--- src/routes/invoice/pdf/+server.ts
+++ src/routes/invoice/pdf/+server.ts
@@ -1,24 +1,43 @@
-import type { RequestHandler } from './$types';
-import { PDFDocument, PDFFont, rgb } from 'pdf-lib';
-import fontkit from '@pdf-lib/fontkit';
 import { createInvoice } from '$lib/pdf/invoice';
-import { convert } from 'pdf-img-convert';
+import type { RequestHandler } from './$types';
+// import { convert } from 'pdf-img-convert';
+import db from '$lib/db/client';
+import { clients, invoices, services } from '$lib/db/schema';
+import { eq } from 'drizzle-orm';
 
-export const GET: RequestHandler = async ({ fetch: localFetch }) => {
+export const GET: RequestHandler = async ({ fetch: localFetch, url }) => {
+    const invoiceId = url.searchParams.get('invoiceId');
+    if (!invoiceId) {
+        return new Response();
+    }
+    const rows = await db
+        .select()
+        .from(invoices)
+        .where(eq(invoices.id, invoiceId))
+        .leftJoin(services, eq(invoices.id, services.invoiceId))
+        .leftJoin(clients, eq(invoices.clientId, clients.id));
+    if (!rows.length) {
+        return new Response();
+    }
+    const invoice = {
+        ...rows[0].invoices,
+        services: rows.map(({ services }) => services),
+        client: rows[0].clients,
+    };
+
     const { pdfDoc, drawClientInfo, drawHeader, drawSummary } = await createInvoice(localFetch);
 
-    await drawHeader({ number: '12345', date: new Date() });
-    await drawClientInfo({ client: 'Client Name', total: 30000 });
+    await drawHeader({ number: invoice.number, date: new Date(invoice.date || '') });
+    await drawClientInfo({
+        client: invoice.client?.opf + '\n' + invoice.client?.name || '',
+        total: 30000,
+    });
     await drawSummary({
-        services: [
-            { title: 'Фигня 1', amount: 1, price: 10000 },
-            { title: 'Item 2', amount: 2, price: 20000 },
-            {
-                title: 'dsakl;dksal; dls;adl;las dsalk;dk;alsk;ld dl;wkdl;s',
-                amount: 1,
-                price: 3213.43,
-            },
-        ],
+        services: invoice.services.map((service) => ({
+            title: service?.title || '',
+            amount: service?.amount || 0,
+            price: parseFloat(service?.price || ''),
+        })),
     });
 
     const pdfBytes = await pdfDoc.save();
