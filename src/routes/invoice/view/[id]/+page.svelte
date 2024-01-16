98bddbb6b2b052693223ef8792eca4cb5463c79f
Kirill <sincerely.manny@gmail.com>
2024-01-16 21:58:23 +0300

    changed link back

--- src/routes/invoice/view/[id]/+page.svelte
+++ src/routes/invoice/view/[id]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+    import Image from 'components/shared/Image.svelte';
+    import { Button } from 'flowbite-svelte';
     import { Download } from 'lucide-svelte';
     import type { PageData } from './$types';
-    import { Button } from 'flowbite-svelte';
-    import Image from 'components/shared/Image.svelte';
 
     export let data: PageData;
 </script>
@@ -16,8 +16,8 @@
             class="flex h-full items-center justify-center object-fill"
         />
         <div class="absolute bottom-3 left-0 flex w-full justify-center">
-            <!-- <a href={`/invoice/pdf?invoiceId=${data.id}`} target="_blank"> -->
-            <a href={`/api/pdf?invoiceId=${data.id}`} target="_blank">
+            <a href={`/invoice/pdf?invoiceId=${data.id}`} target="_blank">
+                <!-- <a href={`/api/pdf?invoiceId=${data.id}`} target="_blank"> -->
                 <Button class="flex gap-1"><Download /> Download PDF</Button>
             </a>
         </div>
